8a1e6dd304c3e308ebbd6ede5256298626585b15
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-20 22:24:44 +0700

    Create Delete and Edit

--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -1,21 +1,18 @@
 <div class="p-4">
-    <form>
+    <form action="/store" method="POST">
+
         <div class="form-group">
             <label for="npm">NPM</label>
             <input type="text" name="npm" class="form-control" id="npm">
         </div>
-
         <div class="form-group">
             <label for="nama">Nama</label>
             <input type="text" name="nama" class="form-control" id="nama">
         </div>
-
         <div class="form-group">
             <label for="alamat">Alamat</label>
             <input type="text" name="alamat" class="form-control" id="alamat">
         </div>
-
         <button type="submit" class="btn btn-primary">Submit</button>
-
     </form>
 </div>
