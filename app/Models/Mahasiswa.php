cae2401f072182555045922e84b44e4d34922380
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-20 22:18:04 +0700

    Add files via upload
    Create Delete and Edit

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table            = 'mahasiswa';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $useTimestamps = true;
+    protected $allowedFields    = ['npm', 'nama', 'alamat'];
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'update_at';
+}
