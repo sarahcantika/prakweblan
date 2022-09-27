3aa423807fc3aab864e6b19ac8f2aa63f0be3194
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-27 18:53:17 +0700

    Create Deskripsi

--- app/Views/mahasiswa/edit.php
+++ app/Views/mahasiswa/edit.php
@@ -1,19 +1,29 @@
+<?= $this->extend('templates/template') ?>
+<?= $this->section('content') ?>
 <div class="p-4">
 
     <form action="/update/<?= $id ?>" method="POST">
-
-        <div class="form-group">
-            <label for="npm">NPM</label>
-            <input type="text" name="npm" class="form-control" id="npm" value="<?= $npm ?>">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="npm">NPM</label>
+                    <input type="text" name="npm" class="form-control" id="npm" value="<?= $npm ?>">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" name="nama" class="form-control" id="nama" value="<?= $nama ?>">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" name="alamat" class="form-control" id="alamat" value="<?= $alamat ?>">
+                </div>
+            </div>
+            <div class="col-6">
+                <label for="deskripsi">Deskripsi</label>
+                <textarea name="deskripsi" class="form-control" id="deskripsi" <?= $deskripsi ?>></textarea>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" name="nama" class="form-control" id="nama" value="<?= $nama ?>">
-        </div>
-        <div class="form-group">
-            <label for="alamat">Alamat</label>
-            <input type="text" name="alamat" class="form-control" id="alamat" value="<?= $alamat ?>">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-palene"></i>Submit</button>
     </form>
 </div>
+<?= $this->endSection() ?>
