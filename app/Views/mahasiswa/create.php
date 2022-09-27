61c668f7ba6270da66e15820858fb3324de69001
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-27 18:52:37 +0700

    Create Deskripsi

--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -1,18 +1,28 @@
+<?= $this->extend('templates/template') ?>
+<?= $this->section('content') ?>
 <div class="p-4">
     <form action="/store" method="POST">
-
-        <div class="form-group">
-            <label for="npm">NPM</label>
-            <input type="text" name="npm" class="form-control" id="npm">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="npm">NPM</label>
+                    <input type="text" name="npm" class="form-control" id="npm">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" name="nama" class="form-control" id="nama">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" name="alamat" class="form-control" id="alamat">
+                </div>
+            </div>
+            <div class="col-6">
+                <label for="deskripsi">Deskripsi</label>
+                <textarea name="deskripsi" class="form-control" id="deskripsi"></textarea>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" name="nama" class="form-control" id="nama">
-        </div>
-        <div class="form-group">
-            <label for="alamat">Alamat</label>
-            <input type="text" name="alamat" class="form-control" id="alamat">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-palene"></i>Submit</button>
     </form>
 </div>
+<?= $this->endSection() ?>
