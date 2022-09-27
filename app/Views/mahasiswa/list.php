c77f938f48de4c217ae19728238c8d4551d9b75a
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-27 18:53:49 +0700

    Create Deskripsi

--- app/Views/mahasiswa/list.php
+++ app/Views/mahasiswa/list.php
@@ -1,4 +1,6 @@
-<a type="button" class="btn btn-primary mb-3" href="/create">Tambah</a>
+<?= $this->extend('templates/template') ?>
+<?= $this->section('content') ?>
+<a type="button" class="btn btn-primary mb-3" href="/create"><i class="fa fa-plus"></i>Tambah</a>
 <table class="table">
     <thead>
         <tr>
@@ -6,7 +8,9 @@
             <th scope="col">NPM</th>
             <th scope="col">Nama</th>
             <th scope="col">Alamat</th>
-            <th scope="col">Created_at</th>
+            <th scope="col">Deskripsi</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Update at</th>
             <th scope="col">Action</th>
         </tr>
     </thead>
@@ -18,13 +22,15 @@
                 <td><?= $mhs['npm'] ?></td>
                 <td><?= $mhs['nama'] ?></td>
                 <td><?= $mhs['alamat'] ?></td>
+                <td><?= $mhs['deskripsi'] ?></td>
                 <td><?= $mhs['created_at'] ?></td>
+                <td><?= $mhs['update_at'] ?></td>
                 <td>
                     <div class="d-flex">
-                        <a href="/edit/<?= $mhs['id'] ?>" class="btn btn-warning mr-3">Edit</a>
+                        <a href="/edit/<?= $mhs['id'] ?>" class="btn btn-warning mr-3"><i class="fa fa-edit"></i>Edit</a>
                         <form action="/delete/<?= $mhs['id'] ?>" method="post">
                             <input name="_method" value="DELETE" type="hidden">
-                            <button type="submit" class="btn btn-danger">Delete</button>
+                            <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i>Delete</button>
                         </form>
                 </td>
             </tr>
@@ -32,3 +38,4 @@
         endforeach ?>
     </tbody>
 </table>
+<?= $this->endSection() ?>
