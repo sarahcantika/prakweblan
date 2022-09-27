4b18e0e974cbeebb7881f8f2f098932fa588a5ef
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-09-27 18:47:06 +0700

    Create Deskripsi

--- app/Controllers/MahasiswaController.php
+++ app/Controllers/MahasiswaController.php
@@ -1,4 +1,6 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Mahasiswa;
@@ -15,9 +17,7 @@ class MahasiswaController extends BaseController
             'mahasiswa' => $mahasiswa
         ];
 
-        return view('templates/header', $data)
-            . view('mahasiswa/list', $data)
-            . view('templates/footer');
+        return view('mahasiswa/list', $data);
     }
 
     public function create()
@@ -26,9 +26,7 @@ class MahasiswaController extends BaseController
             'title' => 'Create Mahasiswa'
         ];
 
-        return view('templates/header', $data)
-            . view('mahasiswa/create', $data)
-            . view('templates/footer');
+        return view('mahasiswa/create', $data);
     }
 
     public function store()
@@ -36,7 +34,8 @@ class MahasiswaController extends BaseController
         if (!$this->validate([
             'npm' => 'required',
             'nama' => 'required',
-            'alamat' => 'required'
+            'alamat' => 'required',
+            'deskripsi' => 'required'
         ])) {
             return redirect()->to('/create');
         }
@@ -45,7 +44,8 @@ class MahasiswaController extends BaseController
         $data = [
             'npm' => $this->request->getPost('npm'),
             'nama' => $this->request->getPost('nama'),
-            'alamat' => $this->request->getPost('alamat')
+            'alamat' => $this->request->getPost('alamat'),
+            'deskripsi' => $this->request->getPost('deskripsi')
         ];
 
         $mahasiswaModel->save($data);
@@ -69,9 +69,7 @@ class MahasiswaController extends BaseController
             'title' => 'Edit Mahasiswa'
         ];
 
-        return view('templates/header', $data)
-            . view('mahasiswa/edit', $mahasiswa)
-            . view('templates/footer');
+        return view('mahasiswa/edit', $data);
     }
 
     public function update($id)
@@ -79,7 +77,8 @@ class MahasiswaController extends BaseController
         if (!$this->validate([
             'npm' => 'required',
             'nama' => 'required',
-            'alamat' => 'required'
+            'alamat' => 'required',
+            'deskripsi' => 'required'
         ])) {
             return redirect()->to('/edit/' . $id);
         }
@@ -88,7 +87,8 @@ class MahasiswaController extends BaseController
         $data = [
             'npm' => $this->request->getVar('npm'),
             'nama' => $this->request->getVar('nama'),
-            'alamat' => $this->request->getVar('alamat')
+            'alamat' => $this->request->getVar('alamat'),
+            'deskripsi' => $this->request->getVar('deskripsi')
         ];
 
         $mahasiswaModel->update($id, $data);
